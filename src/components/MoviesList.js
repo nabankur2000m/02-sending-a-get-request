fe98b43e55414ff46432ebcce56add194bdc129e
NABANKUR MONDAL <76182696+nabankur2000m@users.noreply.github.com>
2024-03-26 22:07:22 +0530

    Storing and deleting the new movies into the Databases

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -6,6 +6,7 @@ import classes from './MoviesList.module.css';
 const MovieList = (props) => {
   return (
     <ul className={classes['movies-list']}>
+      
       {props.movies.map((movie) => (
         <Movie
           key={movie.id} 
